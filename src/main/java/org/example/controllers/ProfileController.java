ec4282663d6a2c78f4c1ce4d1e5cde4f7ff5437d
yarporokh <yaroslavporohnenko@gmail.com>
2022-06-07 00:00:33 +0300

    Finish top-up functionality

--- src/main/java/org/example/controllers/ProfileController.java
+++ src/main/java/org/example/controllers/ProfileController.java
@@ -4,9 +4,12 @@ import org.example.models.User;
 import org.example.services.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -31,10 +34,10 @@ public class ProfileController {
 
     @PreAuthorize("hasAuthority('USER')")
     @PostMapping("/balance")
-    public String topUpBalance(@AuthenticationPrincipal User user, @RequestParam("addBalance") double balance) {
-        System.out.println(user);
-        System.out.println(balance);
-//        userService.saveUserBalance(user, balance);
+    public String topUpBalance(Model model, @RequestParam("addBalance") double balance) {
+        User user = (User) model.getAttribute("user");
+        assert user != null;
+        userService.saveUserBalance(user, balance);
         return "redirect:/user/profile";
     }
 }
