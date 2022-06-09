72730b846a84f03164e2fa3b7ff1b93c7420c950
yarporokh <yaroslavporohnenko@gmail.com>
2022-06-09 12:50:55 +0300

    Added applications constants

--- src/main/java/org/example/utils/ApplicationConstants.java
+++ src/main/java/org/example/utils/ApplicationConstants.java
@@ -10,4 +10,7 @@ public class ApplicationConstants {
     public static final String PAYMENT_STATUS_EXPECTED = "Payment expected";
     public static final String PAYMENT_STATUS_CANCELED = "Canceled";
     public static final String PAYMENT_STATUS_PAID = "Paid";
+
+    //Price
+    public static final double BASE_PRICE = 0.0;
 }
