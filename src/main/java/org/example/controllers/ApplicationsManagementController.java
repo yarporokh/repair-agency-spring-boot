293e12181461670043d0532bcc9e6df9a5357726
yarporokh <yaroslavporohnenko@gmail.com>
2022-06-10 13:00:23 +0300

    Added application management functionality

--- src/main/java/org/example/controllers/ApplicationsManagementController.java
+++ src/main/java/org/example/controllers/ApplicationsManagementController.java
@@ -1,16 +1,19 @@
 package org.example.controllers;
 
 import org.example.models.Application;
+import org.example.models.User;
 import org.example.services.impl.ApplicationService;
 import org.example.services.impl.UserService;
+import org.example.utils.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.example.utils.ApplicationConstants.*;
 
 @Controller
 @PreAuthorize("hasAnyAuthority('MANAGER', 'SERVICEMAN')")
@@ -28,7 +31,55 @@ public class ApplicationsManagementController {
     @GetMapping("/allapps")
     public String getAllApps(Model model) {
         List<Application> applicationList = applicationService.findAll();
+        List<User> servicemanList = userService.findByRole(Role.SERVICEMAN);
+        String[] progressArr = new String[]{PROGRESS_NOT_STARTED, PROGRESS_AT_WORK, PROGRESS_DONE};
+        String[] paymentList = new String[]{PAYMENT_STATUS_EXPECTED, PAYMENT_STATUS_CANCELED, PAYMENT_STATUS_PAID};
+
+        model.addAttribute("payment", paymentList);
+        model.addAttribute("servicemanList", servicemanList);
+        model.addAttribute("progressArr", progressArr);
         model.addAttribute("applicationList", applicationList);
         return "allApplications";
+    }
+
+    @PostMapping("/setPrice")
+    public String setPrice(@RequestParam("setPrice") double price, @RequestParam("appId") Application application) {
+        application.setPrice(price);
+        applicationService.save(application);
+        return "redirect:/management/allapps";
+    }
+
+    @PostMapping("/closeApp")
+    public String closeApp(@RequestParam("appId") Application application) {
+        application.setPaymentStatus(PAYMENT_STATUS_CANCELED);
+        applicationService.save(application);
+        return "redirect:/management/allapps";
+    }
+
+    @PostMapping("/setServiceman")
+    public String setServicemanByManager(@RequestParam("appId") Application application,
+                           @RequestParam("setServiceman") User serviceman) {
+        application.setServiceman(serviceman);
+        applicationService.save(application);
+        return "redirect:/management/allapps";
+    }
+
+    @PostMapping("/claimServiceman")
+    public String claimServiceman(@RequestParam("appId") Application application,
+                                         Model model) {
+        User serviceman = (User) model.getAttribute("user");
+        System.out.println(serviceman);
+        application.setServiceman(serviceman);
+        applicationService.save(application);
+        return "redirect:/management/allapps";
+    }
+
+    @PostMapping("/changeProgress")
+    public String changeProgress(
+            @RequestParam("appId") Application application,
+            @RequestParam("changeProgress") String changeProgress) {
+        application.setProgress(changeProgress);
+        applicationService.save(application);
+        return "redirect:/management/allapps";
     }
 }
